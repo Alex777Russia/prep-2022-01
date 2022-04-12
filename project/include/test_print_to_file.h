e4fd0eec6b1b5123a9fc7553480f4705e12445c9
Alex777Russia <shevalex777@mail.ru>
2022-04-12 17:54:30 +0300

    Выполнена работа над файлами test_print_to_file .c .h

---
+++ project/include/test_print_to_file.h
@@ -0,0 +1,9 @@
+#ifndef PROJECT_INCLUDE_TEST_PRINT_TO_FILE_H_
+#define PROJECT_INCLUDE_TEST_PRINT_TO_FILE_H_
+
+int test_print_person();
+int test_print_transaction();
+
+int test_update_clients();
+
+#endif  // PROJECT_INCLUDE_TEST_PRINT_TO_FILE_H_
